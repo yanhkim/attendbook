1505fb5dc7e3fc05817b771c4a4bae3ea676e22d
Namhoon Kim <yanhkim@gmail.com>
2011-01-19 17:17:01 +0900

    sequential request

---
+++ requester.js
@@ -0,0 +1,201 @@
+function Requester(client, commands) {
+	this.client = client;
+	this.host = client.host;
+	this.commands = commands.reverse();	//for using javascript's own pop|push function
+
+	this.default_header = {
+		'Host': this.host,
+		'Connection': 'keep-alive',
+		'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.237 Safari/534.10',
+		'Accept': 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5',
+		'Accept-Language': 'ko-KR,ko;q=0.8,en-US;q=0.6,en;q=0.4',
+		'Accept-Charset': 'windows-949,utf-8;q=0.7,*;q=0.3'
+	};
+}
+
+Requester.prototype = {
+	run: function() {
+		var command = this.commands.pop();
+		this.requestchain(command);
+	},
+	requestchain: function(c) {
+		if (c === undefined) {
+			console.log('close');
+			return;
+		}
+
+		var header = this.prepareHeader(c);
+		var req = this.client.request(c.method, c.path, header);
+
+		if (c.method == 'POST' && c.body)
+			req.write(c.body);
+
+		req.end();
+
+		//req.on('response', this.bindHandler(req, this.traceHead, this.traceBody, this.connector));
+		req.on('response', this.bindHandler(req, this.headerHandler, this.traceBody, this.connector));
+	},
+	prepareHeader: function(c) {
+		var copy = function(d, s) {
+			if (s === undefined)
+				return;
+
+			for (var p in s)
+				d[p] = s[p];
+		}
+		var h = {};
+		copy(h, this.default_header);
+		copy(h, c.header);
+
+		if (this.commands.length == 0)
+			h['Connection'] = 'close';
+
+		var cookie = this.makeCookieField(this.host, c.path);
+		if (cookie)
+			h['Cookie'] = cookie;
+
+		return h;
+	},
+	makeCookieField: function(host, path) {
+		if (this.cookies == undefined)
+			return;
+
+		var canAccept = function(h, p, c) {		// TODO
+			return true;
+		}
+
+		var ret = '';
+		for (var i in this.cookies) {
+			var cookie = this.cookies[i];
+			if (canAccept(host, path, cookie))
+				ret += cookie['content'] + '; ';
+		}
+
+		ret = ret.substr(0, ret.length - 2);	// remove trailing '; '
+
+		if (ret.length > 0)
+			return ret;
+	},
+	bindHandler: function(r, onHeader, onData, onEnd) {
+		var context = this;
+		return function(r) {
+			if (onHeader)
+				onHeader(context, r);
+
+			if (onData)
+				r.on('data', function(c) {
+					onData(context, c);
+				});
+
+			if (onEnd)
+				r.on('end', function() {
+					onEnd(context);
+				});
+		}
+	},
+	connector: function(context) {
+		context.requestchain(context.commands.pop());
+	},
+	traceHead: function(context, r) {
+		console.log('STATUS: ' + r.statusCode);
+		console.log('HEADERS: ' + JSON.stringify(r.headers));
+
+		//console.log('cookie: ' + r.headers.
+		exports.foo = r.headers;
+
+		r.setEncoding('utf8');
+	},
+	traceBody: function(context, c) {
+		console.log('BODY: ' + c);
+	},
+	headerHandler: function(context, r) {
+		var header = r.headers,
+		rawCookies = header['set-cookie'];
+
+		if (rawCookies == undefined)
+			return;
+
+		var cookies = [];
+		for (var i in rawCookies) {
+			var rawCookie = rawCookies[i].split(';');
+			var rawCookie = rawCookie.map(function(s) { return s.replace(/^\s+|\s+$/g, '') });		// strip LR
+			var cookie = {};
+			for (var j in rawCookie) {
+				var field = rawCookie[j];
+				var pos = field.indexOf('=');
+				var name = field.substr(0, pos);
+				var value = field.substr(pos + 1);
+
+				if (j > 0)
+					cookie[name] = value;
+				else
+					cookie['content'] = field;
+			}
+			cookies.push(cookie);
+		}
+
+		context.cookies ? context.cookies.concat(cookies) : context.cookies = cookies;
+	}
+};
+
+function Command(method, path) {
+	this.method = method;
+	this.path = path;
+	this.header = {};
+}
+
+Command.prototype = {
+	setBody: function(b) {
+		this.header['Content-Length'] = b.length;
+		this.header['Content-Type'] = 'application/x-www-form-urlencoded';
+		this.body = b;
+		return this;
+	},
+	setHeader: function(name, value) {
+		this.header[name] = value;
+		return this;
+	}
+};
+
+exports.createRequester = function(client, commands) {
+	return new Requester(client, commands);
+};
+
+exports.createCommand = function(method, path) {
+	return new Command(method, path);
+}
+
+/*
+var http = require('http');
+var infra = http.createClient(80, 'www.infraware.net');
+
+var commands = [];
+
+var command = new Command('POST', '/MainPage/Login_Company.asp');
+command.setBody('cboCompany=Infraware&txtID=nhkim&txtPasswd=ejaqufk&URLInfo=http%3A%2F%2Fwww.infraware.net%2FMyPage%2FMainFrame.asp%3FRightMenuURL%3Dhttp%3A%2F%2Fwww.infraware.net%2FMyPage%2FMyInfo%2FMyInfoEdit.asp');
+commands.push(command);
+
+command = new Command('POST', '/MainPage/Login_User.asp');
+command.setBody('UserID=nhkim&UserPasswd=ejaqufk&URLInfo=http%3A%2F%2Fwww.infraware.net%2FMyPage%2FMainFrame.asp%3FRightMenuURL%3Dhttp%3A%2F%2Fwww.infraware.net%2FMyPage%2FMyInfo%2FMyInfoEdit.asp');
+commands.push(command);
+
+command = new Command('GET', '/MyPage/MyInfo/MyInfoEdit.asp');
+commands.push(command);
+
+//var foo = new requester(google, [{method: 'GET', path: '/'}, {method: 'GET', path: '/m/search?q=test'}]);
+var foo = new Requester(infra, commands);
+foo.run();
+
+/*
+var request = google.request('GET', '/', {'host': 'www.google.com'});
+request.end();
+request.on('response', function (response) {
+  console.log('STATUS: ' + response.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(response.headers));
+  response.setEncoding('utf8');
+  response.on('data', function (chunk) {
+	  console.log('BODY: ' + chunk);
+  });
+});
+*/
+
