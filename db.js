97281458c85f8c18fb814d4587dc780079604592
Namhoon Kim <yanhkim@gmail.com>
2011-01-21 14:40:44 +0900

    implementing db...

--- db.js
+++ db.js
@@ -1,3 +1,46 @@
+//simple binary lock ^-^
+var lock = (function() {
+	var _lock = false;
+	return {
+		set: function() {
+			_lock = true;
+		},
+		clear: function() {
+			_lock = false;
+			later.replay();		//attempt to deal with delayed jobs, if exists..
+		},
+		p: function() {
+			return _lock;
+		}
+	}
+})();
+
+//simple queue
+var later = (function() {
+	var queue = [];
+	return {
+		register: function(kind, arg1, arg2) {
+			queue.push([kind, arg1, arg2]);
+		},
+		replay: function() {
+			var job = queue.shift();
+			if (job == undefined)
+				return;
+
+			switch (job[0]) {
+				case 'insert':
+					insert(job[1]);
+					break;
+				case 'query':
+					query(job[1], job[2]);
+					break;
+			}
+		}
+	}
+})();
+
+lock.set();		//set lock until db ready
+
 //setup
 var sqlite = require('./deps/node-sqlite/sqlite.js'),
 db = new sqlite.Database();
@@ -10,29 +53,14 @@ var SQL_CREATE_TABLE =
 "CREATE TABLE\n" +
 "records(id TEXT, date DATE, rank INTEGER, grade TEXT, late_rate DOUBLE, avg_facetime TEXT, avg_latetime TEXT, point DOUBLE);";
 
-var SQL_REG_DATE_TRIGGER =
-"CREATE TRIGGER set_date_for_records AFTER INSERT ON records\n" +
-"BEGIN\n" +
-"	UPDATE records SET date = DATETIME('NOW', 'LOCALTIME') WHERE rowid = new.rowid;\n" +
-"END;";
+var SQL_INSERT =
+"INSERT INTO records (id, date, rank, grade, late_rate, avg_facetime, avg_latetime, point)\n" +
+"VALUES ({0}, DATETIME('NOW', 'LOCALTIME'), {1}, {2}, {3}, {4}, {5}, {6});";
 
-//simple binary lock ^-^
-var lock = (function() {
-	var _lock = false;
-	return {
-		set: function() {
-			_lock = true;
-		},
-		clear: function() {
-			_lock = false;
-		},
-		p: function() {
-			return _lock;
-		}
-	}
-})();
+var SQL_QUERY =
+"SELECT date, rank, grade, late_rate, avg_facetime, avg_latetime, point\n" +
+"FROM records WHERE id = {0} ORDER BY date DESC;";
 
-lock.set();
 db.open('attendbook.db', function (error) {
 	if (error) {
 		console.log('db.js: error on open database');
@@ -61,26 +89,82 @@ db.open('attendbook.db', function (error) {
 			//exports.bar = 'create';
 			//exports.bar2 = rows;
 
-			db.execute(SQL_REG_DATE_TRIGGER, function (error, rows) {
-				if (error) {
-					console.log('db.js: error on register trigger');
-					throw error;
-				}
-
-				//exports.foo = 'trigger';
-				//exports.foo2 = rows;
-
-				lock.clear();
-			});
+			lock.clear();
 		});
 	});
 });
 
-//interface
-exports.insert = function(data) {
 
+function insert(data) {
+	var sql = data.fillSQL(SQL_INSERT);
+	lock.set();
+	db.execute(sql, function (error, rows) {
+		lock.clear();	//error or not, clear the lock.
+
+		if (error) {
+			console.log('db.js: error on attempt to insert row - ' + JSON.stringify(data));
+			throw error;
+		}
+	});
+}
+
+function query(key, onData) {
+	var sql = format(SQL_QUERY, escape(key));
+
+	lock.set();
+	db.execute(sql, function (error, rows) {
+		lock.clear();
+
+		if (error) {
+			console.log('db.js: error on attempt to query for - ' + key);
+			throw error;
+		}
+
+		exports.foo = rows;
+	});
+}
+
+function format(s) {
+   	var formatted = s;
+   	for (var i = 1; i < arguments.length; i++) {
+       	var regexp = new RegExp('\\{' + (i - 1) + '\\}', 'gi');
+       	formatted = formatted.replace(regexp, arguments[i]);
+   	}
+   	return formatted;
+}
+
+// features
+function Data(key, rank, grade, lrate, aftime, altime, point) {
+	this.key = escape(key);
+	this.rank = escape(rank);
+	this.grade = escape(grade);
+	this.lrate = escape(lrate);
+	this.aftime = escape(aftime);
+	this.altime = escape(altime);
+	this.point = escape(point);
+}
+
+Data.prototype = {
+	fillSQL: function(template) {
+		return format(template, this.key, this.rank, this.grade, this.lrate, this.aftime, this.altime, this.point);
+	}
 };
-exports.query = function(key, onData) {
 
+exports.insert = function(data) {
+	if (lock.p()) {
+		later.register("insert", data);
+		return;
+	}
+
+	insert(data);
+};
+
+exports.query = function(key, onData) {
+	if (lock.p()) {
+		later.register("query", key, onData);
+		return;
+	}
+
+	query(key, onData);
 };
 
