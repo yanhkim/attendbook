1b0cba8be26533893e59100173dfa74457c076e7
Namhoon Kim <yanhkim@gmail.com>
2011-01-28 16:17:14 +0900

    implements server

---
+++ server.js
@@ -0,0 +1,179 @@
+HOST = null; // localhost
+PORT = 8001;
+
+// when the daemon started
+var starttime = (new Date()).getTime();
+
+var mem = process.memoryUsage();
+// every 10 seconds poll for the memory.
+setInterval(function () {
+  mem = process.memoryUsage();
+}, 10*1000);
+
+
+var fu = require("./fu"),
+    ab = require("./attendbook"),
+    url = require("url");
+
+var SESSION_TIMEOUT = 60 * 1000;
+
+var sessions = {};
+
+function createSession (nick) {
+  for (var i in sessions) {
+    var session = sessions[i];
+    if (session && session.nick === nick) return null;
+  }
+
+  var session = { 
+    nick: nick, 
+    id: Math.floor(Math.random()*99999999999).toString(),
+    timestamp: new Date(),
+
+    poke: function () {
+      session.timestamp = new Date();
+    },
+
+    destroy: function () {
+      channel.appendMessage(session.nick, "part");
+      delete sessions[session.id];
+    }
+  };
+
+  sessions[session.id] = session;
+  return session;
+}
+
+// interval to kill off old sessions
+setInterval(function () {
+  var now = new Date();
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id))
+		continue;
+
+    var session = sessions[id];
+    if (now - session.timestamp > SESSION_TIMEOUT) {
+      session.destroy();
+    }
+  }
+}, 1000);
+
+fu.listen(Number(process.env.PORT || PORT), HOST);
+
+fu.get("/", fu.staticHandler("index.html"));
+fu.get("/style.css", fu.staticHandler("style.css"));
+fu.get("/client.js", fu.staticHandler("client.js"));
+fu.get("/jquery-1.2.6.min.js", fu.staticHandler("jquery-1.2.6.min.js"));
+
+
+fu.get("/who", function (req, res) {
+  var nicks = [];
+  for (var id in sessions) {
+    if (!sessions.hasOwnProperty(id)) continue;
+    var session = sessions[id];
+    nicks.push(session.nick);
+  }
+  res.simpleJSON(200, { nicks: nicks
+                      , rss: mem.rss
+                      });
+});
+
+fu.get("/join", function (req, res) {
+  var q = url.parse(req.url, true).query,
+
+  var session = createSession(q.id);
+  if (session == null) {
+    res.simpleJSON(400, {error: "ID in use"});
+    return;
+  }
+
+  var result = {
+	  sid: session.id,
+	  id: session.nick,
+	  rss: mem.rss,
+	  starttime: starttime
+  };
+
+  ab.login(q.id, q.pwd, function (error) {
+	  if (error) {
+		  var msg = 'login failed with id: ' + q.id;
+		  console.log('server.js: ' + msg);
+		  res.simpleJSON(400, {error: msg});
+		  session.destroy();
+		  throw error;
+	  }
+
+	  if (!q.direct) {
+  		res.simpleJSON(200, result);
+  		session.poke();
+		return;
+	  }
+
+	  ab.query(q.id, function (error, records) {
+		  if (error) {
+			  var msg = 'query faild with id: ' + q.id;
+			  console.log('server.js: ' + msg);
+		      res.simpleJSON(400, {error: msg});
+		  	  session.destroy();
+			  throw error;
+		  }
+
+		  result.records = records;
+		  res.simpleJSON(200, result);
+		  session.poke();
+	  });
+  });
+});
+
+fu.get('/query', function (req, res) {
+	var q = url.parse(req.url, true).query,
+	sid = q.sid,
+	id = q.id;
+
+	var session;
+	if (sid && sessions[sid]) {
+		session = sessions[sid];
+		session.poke();
+	}
+
+	if (session == undefined) {
+		var msg = 'attempt to query with non-exist session id: ' + sid;
+		console.log('server.js: ' + msg);
+		res.simpleJSON(400, {error: msg});
+		return;
+	}
+
+	ab.query(id || session.nick, function (error, records) {
+		if (error) {
+			var msg = 'query faild with id: ' + q.id;
+			console.log('server.js: ' + msg);
+			res.simpleJSON(400, {error: msg});
+			throw error;
+		}
+
+		result.records = records;
+		res.simpleJSON(200, result);
+		session.poke();
+	});
+});
+
+fu.get("/ping", function (req, res) {
+  var id = url.parse(req.url, true).query.id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.poke();
+  }
+  res.simpleJSON(200, { rss: mem.rss });
+});
+
+fu.get("/part", function (req, res) {
+  var id = url.parse(req.url, true).query.id;
+  var session;
+  if (id && sessions[id]) {
+    session = sessions[id];
+    session.destroy();
+  }
+  res.simpleJSON(200, { rss: mem.rss });
+});
+
